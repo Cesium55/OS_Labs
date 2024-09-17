4b9e78bc61f3c1955f4b94230ba9c42172c0e862
root <root@maken139905.vds>
2024-09-17 13:17:38 +0000

    lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,19 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int length = strlen(str);
+
+	for (int i=0;i<length/2;i++){
+		Swap((str+i), (str+length-1-i));
+	}
+}
+
+void Swap(char *left, char *right)
+{
+	char temp = *left;
+	*left = *right;
+	*right = temp;
 }
 
